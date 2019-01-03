72eba524d6488fa0b5a9fb4b6eb26f9bc7fb2672
ipandz00 <ipandz00@gmail.com>
2019-01-03 18:52:20 +0100

    Text overflow fixed

--- frontend-app/src/components/MovieList.js
+++ frontend-app/src/components/MovieList.js
@@ -7,13 +7,13 @@ const MovieList = (props) => {
 			return (
 				<div className="col-md-4" key={item._id}>
 		          <div className="card mb-4 shadow-sm">
-		          	<img src={"https://image.tmdb.org/t/p/w500" + item.poster_path} className="card-img-top" alt="..." />
+		          	<img src={"https://image.tmdb.org/t/p/w500" + item.poster_path} className={"card-img-top " + styles.cardImage} alt="..." />
 		            <div className="card-body">
 		              <h5 className="card-title">{item.title}</h5>
-		              <p className="card-text">{item.overview}</p>
+		              <p className={"card-text " + styles.movieOverview}>{item.overview}</p>
 		              <div className="d-flex justify-content-between align-items-center">
 		                <div className="btn-group">
-		                  <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+		                  <button type="button" className="btn btn-sm btn-info">View</button>
 		                </div>
 		                <small className="text-muted">{item.release_date}</small>
 		              </div>
@@ -26,7 +26,7 @@ const MovieList = (props) => {
 		return movies;
 	}
 	return (
-		<div className='container'>
+		<div className='container mt-3'>
 			<div className='row'>
 				{props.data !== null && 
 					mapMovies()
