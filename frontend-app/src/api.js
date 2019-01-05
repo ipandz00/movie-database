189bb24f47d38a8cc44f8988ec14bcc82ae1d5a5
ipandz00 <ipandz00@gmail.com>
2019-01-05 13:36:37 +0100

    API.js updated with url switcher (prod/dev)

--- frontend-app/src/api.js
+++ frontend-app/src/api.js
@@ -1,8 +1,13 @@
 import axios from 'axios';
+const env = process.env.NODE_ENV || 'development';
+var urlPrefix = '';
+if(env === 'development') {
+	urlPrefix = 'http://localhost:8081/';
+}
 
 export function getMovies(page = 1, size = 9) {
 	return new Promise((resolve, reject) => {
-		axios.get('http://localhost:8081/api/movies',
+		axios.get(urlPrefix + 'api/movies',
 			{params: {
 			    page: page,
 			    size: size
@@ -21,7 +26,7 @@ export function getMovies(page = 1, size = 9) {
 
 export function getMovie(id) {
 	return new Promise((resolve, reject) => {
-		axios.get('http://localhost:8081/api/movies/' + id)
+		axios.get(urlPrefix + 'api/movies/' + id)
 		.then((data) => {
 			if(data.status === 200) {
 				resolve(data.data);
